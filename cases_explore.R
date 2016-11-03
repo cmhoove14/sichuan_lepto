02c355eee0c9201caafa6ff1da2b1ca36774ee0d
cmhoove14 <cmhoove14@gmail.com>
2016-11-03 11:05:54 -0700

    Updates exploring lepto data, mostly on township level

--- cases_explore.R
+++ cases_explore.R
@@ -1,5 +1,8 @@
 #Lepto case data with land cover, population, and SAGA TWI extracted to case locations
+require(lubridate)
+require(raster)
 lep<-read.csv('C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/China/Data/Lepto/cases_twi_pop_globcov.csv')
+twi<-raster('C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/China/Data/Lepto/SL_ST/SL_ST/tiffs/SAGA_TWI1.tif')
 
 #Data cleaning ############
   #convert birthday and case date to dates
@@ -22,6 +25,8 @@ lep<-read.csv('C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/China/Data/
       lep[i,16] = covs[which(code==codes)]
     }
     
+  
+twidf<-as.data.frame(twi)      
 #Get summaries of data ##########
   cases_crop = length(lep$OBJECTID[lep$landcv == "cropland"])
   cases_for = length(lep$OBJECTID[lep$landcv == "forest"])
@@ -31,7 +36,15 @@ lep<-read.csv('C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/China/Data/
   
   cases_tot = sum(cases_crop,cases_for,cases_urb,cases_wat,cases_shrub)
   
-  boxplot(lep$SAGA_TWI)
+  
+plot(density(twidf$SAGA_TWI1, na.rm = TRUE), xlab = 'TWI Value', main = '', lwd = 2)
+  lines(density(lep$SAGA_TWI, na.rm = TRUE), col = 'blue', lwd = 2) 
+  
+boxplot(twidf$SAGA_TWI1, lep$SAGA_TWI, na.rm = TRUE, 
+        ylab = 'TWI', boxwex = 0.25, names = c('All', 'Cases'))
     mean(lep$SAGA_TWI)
     sd(lep$SAGA_TWI)
+    summary(twidf$SAGA_TWI1, na.rm=TRUE)
+    median(twidf$SAGA_TWI1, na.rm=TRUE)
+    quantile(twidf$SAGA_TWI1, na.rm=TRUE)
     
